f5d48cdd9182a1e2d2ad846162d5dd194a7aed0d
waxx567 <waynem567@gmail.com>
2023-02-25 12:07:46 +0200

    Header positioning

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+def main():
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    main()
