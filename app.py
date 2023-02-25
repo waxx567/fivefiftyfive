32ce90a4c89268cbf0ec418a6188f457a9dddd50
waxx567 <waynem567@gmail.com>
2023-02-25 12:26:57 +0200

    Same

--- app.py
+++ app.py
@@ -3,12 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-def main():
-    @app.route("/")
-    def index():
-        return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    main()
+@app.route("/")
+def index():
+    return render_template("index.html")
