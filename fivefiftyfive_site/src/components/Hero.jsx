ade41eedd31258efed6b7292876d05902e81bc01
waxx567 <waynem567@gmail.com>
2023-08-12 19:37:45 +0200

    Update Hero.jsx

--- fivefiftyfive_site/src/components/Hero.jsx
+++ fivefiftyfive_site/src/components/Hero.jsx
@@ -8,8 +8,15 @@ const Hero = () => {
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-4 h-4 rounded-full bg-[#915eff]" />
+          <div className="w-4 h-4 rounded-full bg-[#915EFF]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi,&nbsp;I'm&nbsp;<span className="text-[#915EFF]">Wayne</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user interfaces, and web applications.
+          </p>
         </div>
       </div>
     </section>
