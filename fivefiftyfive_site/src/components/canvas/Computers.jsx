4cbcd436142d9f1e2169631c90849774bf5902f5
waxx567 <waynem567@gmail.com>
2023-08-12 20:23:59 +0200

    Update Computers.jsx

--- fivefiftyfive_site/src/components/canvas/Computers.jsx
+++ fivefiftyfive_site/src/components/canvas/Computers.jsx
@@ -5,8 +5,31 @@ import { OrbitControls, Prreload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf")
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive 
+        object={computer.scene}
+      />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls />
+      </Suspense>
+    </Canvas>
   )
 }
 
