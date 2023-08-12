5c5000233786d36fe05b9ebfca3a280bedf572be
waxx567 <waynem567@gmail.com>
2023-08-12 18:17:28 +0200

    Update Navbar.jsx

--- fivefiftyfive_site/src/components/Navbar.jsx
+++ fivefiftyfive_site/src/components/Navbar.jsx
@@ -59,8 +59,11 @@ const Navbar = () => {
                     active === link.title
                       ? "text-white"
                       : "text-secondary"
-                  } hover:text-white text-[18px] font-medium cursor-pointer`}
-                  onClick={() => setActive(link.title)}
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
                 >
                   <a href={`#${link.id}`}>{link.title}</a>
                 </li>
