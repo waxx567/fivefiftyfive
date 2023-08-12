1c865fb770adfaecbef5c5e6a7ea58c25faf888d
waxx567 <waynem567@gmail.com>
2023-08-12 17:06:47 +0200

    Add logo

--- fivefiftyfive_site/src/components/Navbar.jsx
+++ fivefiftyfive_site/src/components/Navbar.jsx
@@ -22,6 +22,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">Wayne <span>| fivefiftyfive</span></p>
         </Link>
       </div>        
     </nav>
