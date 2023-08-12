0e25155ba42aa7283707fd3a7997b6ffb67a2f3c
waxx567 <waynem567@gmail.com>
2023-08-12 17:24:04 +0200

    Update Navbar.jsx

--- fivefiftyfive_site/src/components/Navbar.jsx
+++ fivefiftyfive_site/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Wayne <span>| fivefiftyfive</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer">Wayne <span>| fivefiftyfive.io</span></p>
         </Link>
       </div>        
     </nav>
