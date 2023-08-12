8c1cfc71f89de0ce08861f24d223f4121899c81f
waxx567 <waynem567@gmail.com>
2023-08-12 13:54:45 +0200

    Update App.jsx

--- fivefiftyfive_site/src/App.jsx
+++ fivefiftyfive_site/src/App.jsx
@@ -4,13 +4,23 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover b-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
       </div>
-      </BrowserRouter>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
 
