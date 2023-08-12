82147692122a0bb0fb940485ee77322ab2af2fae
waxx567 <waynem567@gmail.com>
2023-08-12 19:42:24 +0200

    Update Hero.jsx

--- fivefiftyfive_site/src/components/Hero.jsx
+++ fivefiftyfive_site/src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi,&nbsp;I'm&nbsp;<span className="text-[#915EFF]">Wayne</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user interfaces, and web applications.
+            I develop 3D visuals, user <br className="sm:block hidden" /> interfaces, and web applications.
           </p>
         </div>
       </div>
