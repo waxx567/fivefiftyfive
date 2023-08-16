ca91b06bf108c2d19aa9bc06ed1630eaa0090652
waxx567 <waynem567@gmail.com>
2023-08-16 11:55:42 +0200

    Start again

--- project/static/js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('form').addEventListener('submit', function(event){
-        let email = document.querySelector('#email').value;
-        alert('test process ' + email);
-        event.preventDefault();
-    });
-});
