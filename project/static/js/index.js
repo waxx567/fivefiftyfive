9e54b7d1350f2d7dc363edd39545848ed2f7105e
waxx567 <waynem567@gmail.com>
2023-08-17 20:26:04 +0200

    Update index.js

---
+++ project/static/js/index.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// when hamburger button is clicked
+navToggle.addEventListener("click", () => {
+
+    const visiblity = nav.getAttribute("data-visible");
+    // open nav if closed
+    
+    // close nav if open
+})
+
+
+
