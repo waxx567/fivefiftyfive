97edba826d5bdb922a151fbe558a0a56fddfdf00
waxx567 <waynem567@gmail.com>
2023-08-21 14:54:55 +0200

    Update index.js

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -17,14 +17,19 @@ navToggle.addEventListener("click", () => {
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+let tabFocus = 0;
+
 tabList.addEventListener('keydown', (e) => {
-    const keydownLeft = 37;
-    const keydownRight = 39;
-    
-    // change the tabindex of the current tab to -1
-    
-    // if the right key is pushed, move to the next tab on the right
-    
-    // if the left key is pushed, move to the next tab on the left
+  const keydownLeft = 37;
+  const keydownRight = 39;
+  
+  // change the tabindex of the current tab to -1
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+      tabs[tabFocus].setAttribute("tabindex", -1);
+  }
+  
+  // if the right key is pushed, move to the next tab on the right
+  
+  // if the left key is pushed, move to the next tab on the left
 })
 
