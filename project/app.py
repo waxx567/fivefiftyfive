39f2be809100c70950f04e3229756ca69b298870
waxx567 <waynem567@gmail.com>
2023-08-22 15:43:13 +0200

    Initiate database

--- project/app.py
+++ project/app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template, request
+import sqlite3
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 import sqlite3
 
+# Configure application
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+connection = sqlite3.connect("users.db")
+db = connection.cursor()
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 @app.route("/")
