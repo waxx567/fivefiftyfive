054fee1ee83003cc49ce20d8b4ce6425a20d1586
waxx567 <waynem567@gmail.com>
2023-09-11 07:42:07 +0200

    Update app.py

--- api/gas_api/app.py
+++ api/gas_api/app.py
@@ -7,7 +7,11 @@ from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
-app.config.from_pyfile("config.py")
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///users.db")
