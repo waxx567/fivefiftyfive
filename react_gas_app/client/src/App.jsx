bd46504dd69dc4570e6dc9e0f25f0e5c22f85e7e
waxx567 <waynem567@gmail.com>
2023-09-19 13:59:40 +0200

    Update App.jsx

--- react_gas_app/client/src/App.jsx
+++ react_gas_app/client/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Dashboard from './components/Dashboard/Dashboard'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 
 function App() {
   return (
     <div>
-      App  
+      <Dashboard/>
+      <Login/>
+      <Register/>
     </div>
   )
 }
