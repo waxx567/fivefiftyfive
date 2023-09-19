65127bac21c13d45bf6c3e92838864c7ecc7b647
waxx567 <waynem567@gmail.com>
2023-09-19 15:46:25 +0200

    Update App.jsx

--- react_gas_app/client/src/App.jsx
+++ react_gas_app/client/src/App.jsx
@@ -3,12 +3,31 @@ import Dashboard from './components/Dashboard/Dashboard'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 
+// Import router dom
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Login/></div>
+  },
+  {
+    path: '/register',
+    element: <div><Register/></div>
+  },
+  {
+    path: '/dashboard',
+    element: <div><Dashboard/></div>
+  }
+])
+
 function App() {
   return (
     <div>
-      <Dashboard/>
-      <Login/>
-      <Register/>
+      <RouterProvider></RouterProvider>
     </div>
   )
 }
