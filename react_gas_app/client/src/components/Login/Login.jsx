839499546d9bd072dd063cf382b66dc0ab337b4a
waxx567 <waynem567@gmail.com>
2023-09-19 15:58:15 +0200

    Update Login.jsx

--- react_gas_app/client/src/components/Login/Login.jsx
+++ react_gas_app/client/src/components/Login/Login.jsx
@@ -4,8 +4,9 @@ import './Login.css'
 const Login = () => {
   return (
     <div>
-      <button>To Register</button>
-      <button>To Dashboard</button>
+      <a href='/register'>To Register</a>
+      <a href='/dashboard'>To Dashboard</a>
+      This is the Login Page
     </div>
   )
 }
