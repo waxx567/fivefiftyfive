1fa8f37f6e5794206f7651f98d097984c6033a57
waxx567 <waynem567@gmail.com>
2023-09-20 09:56:30 +0200

    Update Login.jsx

--- react_gas_app/client/src/components/Login/Login.jsx
+++ react_gas_app/client/src/components/Login/Login.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Login.css'
 
+// Import assets
+import video from '../../loginAssets/owl.mp4'
+
 const Login = () => {
   return (
     <div className='loginPage flex'>
       <div className='container flex'>
-        <video src=''></video>
+        <div className='videoDiv'>
+          <video src={video}></video>
+        </div>
       </div>
     </div>
   )
