5f22686c179b5c37bdbc091e9c258eee7711a7bd
waxx567 <waynem567@gmail.com>
2023-09-20 10:31:28 +0200

    Update Login.jsx

--- react_gas_app/client/src/components/Login/Login.jsx
+++ react_gas_app/client/src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom'
 
 // Import assets
 import video from '../../loginAssets/owl.mp4'
@@ -10,6 +11,17 @@ const Login = () => {
       <div className='container flex'>
         <div className='videoDiv'>
           <video src={video} autoPlay muted loop></video>
+          <div className='textDiv'>
+            <h2 className='title'>Delivering Replacement Gas Bottles To Your Door</h2>
+            <p>We're the wise choice for convenience!</p>
+          </div>
+
+          <div className='footerDiv flex'>
+            <span className='text'>Not registered yet?</span>
+            <Link to={'/register'}>
+            <button className='btn'>Sign Up</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
