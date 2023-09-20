30a2509275280618e46da845305bc52519acc06b
waxx567 <waynem567@gmail.com>
2023-09-20 15:41:10 +0200

    Update Login.jsx

--- react_gas_app/client/src/components/Login/Login.jsx
+++ react_gas_app/client/src/components/Login/Login.jsx
@@ -17,57 +17,56 @@ const Login = () => {
       <div className='container flex'>
 
         <div className='videoDiv'>
-            <video src={video} autoPlay muted loop></video>
+        <video src={video} autoPlay muted loop></video>
 
-            <div className='textDiv'>
-              <h2 className='title'>Gas Bottles To Your Door</h2>
-              <p>The wise choice for convenience!</p>
+        <div className='textDiv'>
+          <h2 className='title'>Gas Bottles To Your Door</h2>
+          <p>The wise choice for convenience!</p>
+        </div>
 
-              <div className='footerDiv flex'>
-                <span className='text'>Not registered yet?</span>
-                <Link to={'/register'}>
-                <button className='btn'>Sign Up</button>
-                </Link>
-              </div>
-            </div>
-
+        <div className='footerDiv flex'>
+          <span className='text'>Not registered yet?</span>
+          <Link to={'/register'}>
+          <button className='btn'>Sign Up</button>
+          </Link>
+        </div>
         </div>
         
         <div className='formDiv flex'>
-            <div className='headerDiv'>
-                <img src={logo} alt='Logo' />
-                <h3>Welcome Back!</h3>
+          <div className='headerDiv'>
+            <img src={logo} alt='Logo' />
+            <h3>Welcome Back!</h3>
+          </div>
+
+          <form action='' className='form grid'>
+            <span>Login Status will appear here</span>
+
+            <div className='inputDiv'>
+            <label htmlFor='username'>Username</label>
+            <div className='input flex'>
+              <FaUserShield className='icon'/>
+              <input type='text' id='username' placeholder='Enter Username'></input>
+            </div>
             </div>
 
-            <form action='' className='form grid'>
-                <span>Login Status will appear here</span>
+            <div className='inputDiv'>
+            <label htmlFor='password'>Password</label>
+            <div className='input flex'>
+              <BsFillShieldLockFill className='icon'/>
+              <input type='password' id='password' placeholder='Enter Password'></input>
+            </div>
+            </div>
 
-                <div className='inputDiv'>
-                <label htmlFor='username'>Username</label>
-                <div className='input flex'>
-                    <FaUserShield className='icon'/>
-                    <input type='text' id='username' placeholder='Enter Username'></input>
-                </div>
-                </div>
+          <button type='submit' className='btn flex'>
+          <span>Login</span>
+          <AiOutlineSwapRight className='icon'/>
+          </button>
 
-                <div className='inputDiv'>
-                <label htmlFor='password'>Password</label>
-                <div className='input flex'>
-                    <BsFillShieldLockFill className='icon'/>
-                    <input type='password' id='password' placeholder='Enter Password'></input>
-                </div>
-                </div>
+          <span className='forgotPassword'>
+            Forgot your password? <a href=''>Click Here</a>
+          </span>
 
-                <button type='submit' className='btn flex'>
-                <span>Login</span>
-                <AiOutlineSwapRight className='icon'/>
-                </button>
-
-                <span className='forgotPassword'>
-                Forgot your password? <a href=''>Click Here</a>
-                </span>
-
-            </form>
+        </form>
         </div>
       
       </div>
