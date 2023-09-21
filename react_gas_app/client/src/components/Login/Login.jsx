56e939ab1c5f2ff96881b2e0c5e9625838048731
waxx567 <waynem567@gmail.com>
2023-09-21 07:39:00 +0200

    Update Login.jsx

--- react_gas_app/client/src/components/Login/Login.jsx
+++ react_gas_app/client/src/components/Login/Login.jsx
@@ -39,33 +39,35 @@ const Login = () => {
           </div>
 
           <form action='' className='form grid'>
-            
-            <span>Login Status will appear here</span>
+            <div className='form-gap'>
+                <span>Login Status will appear here</span>
 
-            <div className='inputDiv'>
-            <label htmlFor='username'>Username</label>
-            <div className='input flex'>
-              <FaUserShield className='icon'/>
-              <input type='text' id='username' placeholder='Enter Username'></input>
+                <div className='inputDiv'>
+                <label htmlFor='username'>Username</label>
+                <div className='input flex'>
+                <FaUserShield className='icon'/>
+                <input type='text' id='username' placeholder='Enter Username'></input>
+                </div>
+                </div>
+
+                <div className='inputDiv'>
+                <label htmlFor='password'>Password</label>
+                <div className='input flex'>
+                    <BsFillShieldLockFill className='icon'/>
+                    <input type='password' id='password' placeholder='Enter Password'></input>
+                </div>
+                </div>
+
+                <button type='submit' className='btn flex'>
+                <span>Login</span>
+                <AiOutlineSwapRight className='icon'/>
+                </button>
+
+                <span className='forgotPassword'>
+                Forgot your password? <a href=''>Click Here</a>
+                </span>
+
             </div>
-            </div>
-
-            <div className='inputDiv'>
-              <label htmlFor='password'>Password</label>
-              <div className='input flex'>
-                <BsFillShieldLockFill className='icon'/>
-                <input type='password' id='password' placeholder='Enter Password'></input>
-              </div>
-            </div>
-
-            <button type='submit' className='btn flex'>
-              <span>Login</span>
-              <AiOutlineSwapRight className='icon'/>
-            </button>
-
-            <span className='forgotPassword'>
-              Forgot your password? <a href=''>Click Here</a>
-            </span>
 
           </form>
         </div>
