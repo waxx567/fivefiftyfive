49afe54483d186c858407925c22b17b16c807332
waxx567 <waynem567@gmail.com>
2023-09-22 10:56:21 +0200

    Update Sidebar.jsx

--- react_dash/src/Components/Sidebar Section/Sidebar.jsx
+++ react_dash/src/Components/Sidebar Section/Sidebar.jsx
@@ -8,6 +8,7 @@ import logo from '../../Assets/owl.png'
 import { IoMdSpeedometer } from 'react-icons/io'
 import { MdDeliveryDining, MdOutlineExplore } from 'react-icons/md'
 import { BsTrophy } from 'react-icons/bs'
+import { AiOutlinePieChart } from 'react-icons/ai'
 
 const Sidebar = () => {
   return (
@@ -65,6 +66,56 @@ const Sidebar = () => {
 
         </ul>
       </div>
+
+      <div className='settingsDiv'>
+        <h3 className='divTitle'>
+          SETTINGS
+        </h3>
+        <ul className='menuLists grid'>
+
+          {/* Charts */}
+          <li className='listItem'>
+            <a href='#' className='menuLink flex'>
+              <AiOutlinePieChart className='icon'/>
+              <span className='smallText'>
+                Charts
+              </span>
+            </a>
+          </li>
+
+          {/* My Orders */}
+          <li className='listItem'>
+            <a href='#' className='menuLink flex'>
+              <MdDeliveryDining className='icon'/>
+              <span className='smallText'>
+                My Orders
+              </span>
+            </a>
+          </li>
+
+          {/* Explore */}
+          <li className='listItem'>
+            <a href='#' className='menuLink flex'>
+              <MdOutlineExplore className='icon'/>
+              <span className='smallText'>
+                Explore
+              </span>
+            </a>
+          </li>
+
+          {/* Products */}
+          <li className='listItem'>
+            <a href='#' className='menuLink flex'>
+              <BsTrophy className='icon'/>
+              <span className='smallText'>
+                Products
+              </span>
+            </a>
+          </li>
+
+        </ul>
+      </div>
+
     </div>
   )
 }
