e2a21ca96b1149eb7c6a858771278a669406ac45
waxx567 <waynem567@gmail.com>
2023-09-22 08:57:05 +0200

    Sidebar

--- react_dash/src/Components/Sidebar Section/Sidebar.jsx
+++ react_dash/src/Components/Sidebar Section/Sidebar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './sidebar.css'
 
+// Import images
+import logo from '../../Assets/owl.png'
+
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
+    <div className='sidebar grid'>
+      <div className='logoDiv flex'>
+        <img src={logo} alt='Logo' />
+        <h2>GoGASGo</h2>
+      </div>
     </div>
   )
 }
