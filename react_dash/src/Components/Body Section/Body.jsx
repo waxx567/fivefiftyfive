b6baeb7c2bb20993a50eb14326394367df1472af
waxx567 <waynem567@gmail.com>
2023-09-22 14:32:05 +0200

    Update Body.jsx

--- react_dash/src/Components/Body Section/Body.jsx
+++ react_dash/src/Components/Body Section/Body.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './body.css'
+import Top from './Top Section/Top'
 
 const Body = () => {
   return (
-    <div>
-      Body
+    <div className='mainContent'>
+      <Top/>>
     </div>
   )
 }
