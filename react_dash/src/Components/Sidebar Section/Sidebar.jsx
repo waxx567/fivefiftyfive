f0494c0a122d2f33679f141d340122ac1cde3015
waxx567 <waynem567@gmail.com>
2023-09-22 09:02:46 +0200

    Update Sidebar.jsx

--- react_dash/src/Components/Sidebar Section/Sidebar.jsx
+++ react_dash/src/Components/Sidebar Section/Sidebar.jsx
@@ -11,6 +11,21 @@ const Sidebar = () => {
         <img src={logo} alt='Logo' />
         <h2>GoGASGo</h2>
       </div>
+
+      <div className='menuDiv'>
+        <h3 className='divTitle'>
+          QUICK MENU
+        </h3>
+        <ul className='menuLists grid'>
+
+          <li className='listItem'>
+            <a href='#' className='menuLink flex'>
+              icon
+            </a>
+          </li>
+
+        </ul>
+      </div>
     </div>
   )
 }
