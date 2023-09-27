13986432b966d0a12c183ba296d29e1e54e5cc42
waxx567 <waynem567@gmail.com>
2023-09-27 17:55:01 +0200

    Update Listing.jsx

--- react_dash/src/Components/Body Section/Listing Section/Listing.jsx
+++ react_dash/src/Components/Body Section/Listing Section/Listing.jsx
@@ -9,6 +9,7 @@ import { AiFillHeart } from 'react-icons/ai'
 import img from '../../../Assets/gas_18.png'
 import img1 from '../../../Assets/gas_09.png'
 import img2 from '../../../Assets/gas_12.png'
+import img3 from '../../../Assets/gas_05.png'
 
 const Listing = () => {
   return (
@@ -36,6 +37,12 @@ const Listing = () => {
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
           <img src={img2} alt='18Kg red gas bottle' />
+          <h3>Annual Vince</h3>
+        </div>
+
+        <div className='singleItem'>
+          <AiFillHeart className='icon'/>
+          <img src={img3} alt='18Kg red gas bottle' />
           <h3>Annual Vince</h3>
         </div>
 
