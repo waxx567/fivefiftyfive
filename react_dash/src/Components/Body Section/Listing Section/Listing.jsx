8d99e5ef7f7a218b426518e66a1dd59e71653948
waxx567 <waynem567@gmail.com>
2023-09-27 16:55:24 +0200

    Update Listing.jsx

--- react_dash/src/Components/Body Section/Listing Section/Listing.jsx
+++ react_dash/src/Components/Body Section/Listing Section/Listing.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './listing.css'
 
-// Import icon
+// Import icons
 import { BsArrowRightShort } from 'react-icons/bs'
+import { AiFillHeart } from 'react-icons/ai'
+
+// Import images
+import img from '../../../Assets/gas_18.png'
 
 const Listing = () => {
   return (
@@ -10,8 +14,15 @@ const Listing = () => {
       <div className='heading flex'>
         <h1>My Listing</h1>
         <button className='btn flex'>
-            See All <BsArrowRightShort className='icon'/>
+          See All <BsArrowRightShort className='icon'/>
         </button>
+      </div>
+
+      <div className='secContainer flex'>
+        <div className='singleItem'>
+          <AiFillHeart className='icon'/>
+          <img src={img} alt='' />
+        </div>
       </div>
     </div>
   )
