db753d32728cfdef2694884a789e5f9fdac4eaf8
waxx567 <waynem567@gmail.com>
2023-09-27 16:40:50 +0200

    Update Listing.jsx

--- react_dash/src/Components/Body Section/Listing Section/Listing.jsx
+++ react_dash/src/Components/Body Section/Listing Section/Listing.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './listing.css'
 
+// Import icon
+import { BsArrowRightShort } from 'react-icons/bs'
+
 const Listing = () => {
   return (
-    <div>
-      Listing
+    <div className='listingSection'>
+      <div className='heading flex'>
+        <h1>My Listing</h1>
+        <button className='btn flex'>
+            See All <BsArrowRightShort className='icon'/>
+        </button>
+      </div>
     </div>
   )
 }
