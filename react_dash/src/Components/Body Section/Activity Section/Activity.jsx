7c9e2b7c098af04204da869e5043977dcd00b652
waxx567 <waynem567@gmail.com>
2023-09-28 17:08:37 +0200

    Update Activity.jsx

--- react_dash/src/Components/Body Section/Activity Section/Activity.jsx
+++ react_dash/src/Components/Body Section/Activity Section/Activity.jsx
@@ -6,6 +6,9 @@ import { BsArrowRightShort } from 'react-icons/bs'
 
 // Import images
 import img from '../../../Assets/user_1.jpg'
+import img1 from '../../../Assets/user.jpg'
+import img2 from '../../../Assets/user_3.jpg'
+import img3 from '../../../Assets/user_2.jpg'
 
 const Activity = () => {
   return (
@@ -20,6 +23,7 @@ const Activity = () => {
       </div>
 
       <div className='secContainer grid'>
+
         <div className='singleCustomer flex'>
           <img src={img} alt='Customer' />
           <div className='customerDetails'>
@@ -28,6 +32,34 @@ const Activity = () => {
           </div>
           <div className='duration'>2 min ago</div>
         </div>
+
+        <div className='singleCustomer flex'>
+          <img src={img1} alt='Customer' />
+          <div className='customerDetails'>
+            <span className='name'>Jane Doe</span>
+            <small>Ordered 1 x 12Kg</small>
+          </div>
+          <div className='duration'>21 min ago</div>
+        </div>
+
+        <div className='singleCustomer flex'>
+          <img src={img2} alt='Customer' />
+          <div className='customerDetails'>
+            <span className='name'>Jane Doe</span>
+            <small>Ordered 1 x 18Kg</small>
+          </div>
+          <div className='duration'>47 min ago</div>
+        </div>
+
+        <div className='singleCustomer flex'>
+          <img src={img3} alt='Customer' />
+          <div className='customerDetails'>
+            <span className='name'>Jane Doe</span>
+            <small>Ordered 2 x 9Kg</small>
+          </div>
+          <div className='duration'>67 min ago</div>
+        </div>
+
       </div>
     </div>
   )
