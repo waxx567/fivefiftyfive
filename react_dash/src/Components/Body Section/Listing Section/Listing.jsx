2dc6090994f34e242f40afedc6e2e421b2ee0bb8
waxx567 <waynem567@gmail.com>
2023-09-28 08:27:19 +0200

    Update Listing.jsx

--- react_dash/src/Components/Body Section/Listing Section/Listing.jsx
+++ react_dash/src/Components/Body Section/Listing Section/Listing.jsx
@@ -6,16 +6,16 @@ import { BsArrowRightShort } from 'react-icons/bs'
 import { AiFillHeart } from 'react-icons/ai'
 
 // Import images
-import img from '../../../Assets/gas_18.png'
+import img from '../../../Assets/gas_05.png'
 import img1 from '../../../Assets/gas_09.png'
 import img2 from '../../../Assets/gas_12.png'
-import img3 from '../../../Assets/gas_05.png'
+import img3 from '../../../Assets/gas_18.png'
 
 const Listing = () => {
   return (
     <div className='listingSection'>
       <div className='heading flex'>
-        <h1>My Listing</h1>
+        <h1>Products</h1>
         <button className='btn flex'>
           See All <BsArrowRightShort className='icon'/>
         </button>
@@ -24,26 +24,26 @@ const Listing = () => {
       <div className='secContainer flex'>
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
-          <img src={img} alt='18Kg red gas bottle' />
-          <h3>Annual Vince</h3>
+          <img src={img} alt='5Kg brown gas bottle' />
+          <h3>5Kg bottle</h3>
         </div>
 
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
-          <img src={img1} alt='18Kg red gas bottle' />
-          <h3>Annual Vince</h3>
+          <img src={img1} alt='9Kg purple gas bottle' />
+          <h3>9Kg bottle</h3>
         </div>
 
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
-          <img src={img2} alt='18Kg red gas bottle' />
-          <h3>Annual Vince</h3>
+          <img src={img2} alt='12Kg yellow gas bottle' />
+          <h3>12Kg bottle</h3>
         </div>
 
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
           <img src={img3} alt='18Kg red gas bottle' />
-          <h3>Annual Vince</h3>
+          <h3>18Kg bottle</h3>
         </div>
 
       </div>
