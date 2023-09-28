c327bb225aa0d306da3d41e1ff8ee790b8fc1f9f
waxx567 <waynem567@gmail.com>
2023-09-28 08:51:43 +0200

    Update Listing.jsx

--- react_dash/src/Components/Body Section/Listing Section/Listing.jsx
+++ react_dash/src/Components/Body Section/Listing Section/Listing.jsx
@@ -6,10 +6,11 @@ import { BsArrowRightShort } from 'react-icons/bs'
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 
 // Import images
-import img from '../../../Assets/gas_05.png'
-import img1 from '../../../Assets/gas_09.png'
-import img2 from '../../../Assets/gas_12.png'
-import img3 from '../../../Assets/gas_18.png'
+import gas05 from '../../../Assets/gas_05.png'
+import gas09 from '../../../Assets/gas_09.png'
+import gas12 from '../../../Assets/gas_12.png'
+import gas18 from '../../../Assets/gas_18.png'
+import user from '../../../Assets/user.jpg'
 
 const Listing = () => {
   return (
@@ -27,28 +28,28 @@ const Listing = () => {
         {/* 5Kg bottle */}
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
-          <img src={img} alt='5Kg brown gas bottle' />
+          <img src={gas05} alt='5Kg brown gas bottle' />
           <h3>5Kg bottle</h3>
         </div>
 
         {/* 9Kg bottle */}
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
-          <img src={img1} alt='9Kg purple gas bottle' />
+          <img src={gas09} alt='9Kg purple gas bottle' />
           <h3>9Kg bottle</h3>
         </div>
 
         {/* 12Kg bottle */}
         <div className='singleItem'>
           <AiOutlineHeart className='icon'/>
-          <img src={img2} alt='12Kg yellow gas bottle' />
+          <img src={gas12} alt='12Kg yellow gas bottle' />
           <h3>12Kg bottle</h3>
         </div>
 
         {/* 18Kg bottle */}
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
-          <img src={img3} alt='18Kg red gas bottle' />
+          <img src={gas18} alt='18Kg red gas bottle' />
           <h3>18Kg bottle</h3>
         </div>
 
@@ -56,11 +57,18 @@ const Listing = () => {
 
       <div className='sellers flex'>
         <div className='topSellers'>
+
           <div className='heading flex'>
             <h3>Top Sellers</h3>
             <button className='btn flex'>
               See All <BsArrowRightShort className='icon'/>
             </button>
+          </div>
+
+          <div className='card flex'>
+            <div className='users'>
+              <img src={user} alt='User' />
+            </div>
           </div>
         </div>
       </div>
