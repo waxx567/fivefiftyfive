47a932eae2fcd970f4970c229811df1d77cc87dc
waxx567 <waynem567@gmail.com>
2023-09-28 08:41:28 +0200

    Update Listing.jsx

--- react_dash/src/Components/Body Section/Listing Section/Listing.jsx
+++ react_dash/src/Components/Body Section/Listing Section/Listing.jsx
@@ -3,7 +3,7 @@ import './listing.css'
 
 // Import icons
 import { BsArrowRightShort } from 'react-icons/bs'
-import { AiFillHeart } from 'react-icons/ai'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 
 // Import images
 import img from '../../../Assets/gas_05.png'
@@ -14,6 +14,7 @@ import img3 from '../../../Assets/gas_18.png'
 const Listing = () => {
   return (
     <div className='listingSection'>
+
       <div className='heading flex'>
         <h1>Products</h1>
         <button className='btn flex'>
@@ -22,30 +23,46 @@ const Listing = () => {
       </div>
 
       <div className='secContainer flex'>
+
+        {/* 5Kg bottle */}
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
           <img src={img} alt='5Kg brown gas bottle' />
           <h3>5Kg bottle</h3>
         </div>
 
+        {/* 9Kg bottle */}
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
           <img src={img1} alt='9Kg purple gas bottle' />
           <h3>9Kg bottle</h3>
         </div>
 
+        {/* 12Kg bottle */}
         <div className='singleItem'>
-          <AiFillHeart className='icon'/>
+          <AiOutlineHeart className='icon'/>
           <img src={img2} alt='12Kg yellow gas bottle' />
           <h3>12Kg bottle</h3>
         </div>
 
+        {/* 18Kg bottle */}
         <div className='singleItem'>
           <AiFillHeart className='icon'/>
           <img src={img3} alt='18Kg red gas bottle' />
           <h3>18Kg bottle</h3>
         </div>
 
+      </div>
+
+      <div className='sellers flex'>
+        <div className='topSellers'>
+          <div className='heading flex'>
+            <h3>Top Sellers</h3>
+            <button className='btn flex'>
+              See All <BsArrowRightShort className='icon'/>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
