7833297b91622c488eac4ef742dcf1ee9fbe4009
waxx567 <waynem567@gmail.com>
2023-09-28 16:29:33 +0200

    Update Activity.jsx

--- react_dash/src/Components/Body Section/Activity Section/Activity.jsx
+++ react_dash/src/Components/Body Section/Activity Section/Activity.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './activity.css'
+import { BsArrowRightShort } from 'react-icons/bs'
 
 const Activity = () => {
   return (
-    <div>
-      Activity
+    <div className='activitySection'>
+      <div className='heading flex'>
+        <h1>Recent Activity</h1>
+        <button className='btn flex'>
+          See All
+          <BsArrowRightShort className='icon'/>
+        </button>
+      </div>
     </div>
   )
 }
