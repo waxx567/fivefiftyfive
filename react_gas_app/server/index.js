2c0c62dfa640f5942ea7760eac889b1d58939db2
waxx567 <waynem567@gmail.com>
2023-10-01 09:48:37 +0200

    Update index.js

--- react_gas_app/server/index.js
+++ react_gas_app/server/index.js
@@ -1,4 +1,10 @@
+// Dependencies
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
 const cors = require('cors')
+
+// Run server
+app.listen(3002, () => {
+  console.log('Server is running on port 3002')
+})
