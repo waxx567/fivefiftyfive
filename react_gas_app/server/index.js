f88e4495754867a37e46f317c34a60b05ae25d8f
waxx567 <waynem567@gmail.com>
2023-10-01 13:13:16 +0200

    Update index.js

--- react_gas_app/server/index.js
+++ react_gas_app/server/index.js
@@ -21,4 +21,20 @@ const db = mysql.createConnection({
 // Route to server that registers user
 app.post('/register', (req, res) => {
   // Get variables sent from form
+  const sentEmail = req.body.Email
+  const sentUserName = req.body.UserName
+  const sentPassword = req.body.Password
+
+  // Insert new user into users database
+  const SQL = 'INSERT INTO users (email, username, password) VALUES (?, ?, ?)'
+  const Values = [sentEmail, sentUserName, sentPassword]
+  // Execute SQL statement
+  db.query(SQL, Values, (err, results)=>{
+    if(err){
+      res.send(err)
+    }
+    else{
+      console.log('User inserted successfully')
+    }
+  })
 })
