e1a0d1c52bbde761d7d3bca13b90f84ab6818f78
waxx567 <waynem567@gmail.com>
2023-10-03 13:47:28 +0200

    Update Login.jsx

--- react_gas_app/client/src/components/Login/Login.jsx
+++ react_gas_app/client/src/components/Login/Login.jsx
@@ -23,6 +23,8 @@ const Login = () => {
   // onClick to get what user has entered
   const loginUser = () => {
 
+    // Prevent submitting
+    e.preventDefault();
     // Create API to connect to server
     Axios.post('http://localhost:3002/login', {
       // Create variable to send to server through route
