74f750344003fe3d1b3a6a5ab906e87f88acabac
waxx567 <waynem567@gmail.com>
2023-10-04 10:50:47 +0200

    Update Login.jsx

--- react_gas_app/client/src/components/Login/Login.jsx
+++ react_gas_app/client/src/components/Login/Login.jsx
@@ -126,8 +126,6 @@ const Login = () => {
             <AiOutlineSwapRight className='icon'/>
             </button>
 
-            {/* <a href='/dashboard'>Admin</a> */}
-
           </form>
         </div>
       </div>     
