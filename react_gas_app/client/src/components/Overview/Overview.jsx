5f224984e13e9979d0a102d869ab374a19d49273
waxx567 <waynem567@gmail.com>
2023-10-04 16:41:27 +0200

    Update Overview.jsx

--- react_gas_app/client/src/components/Overview/Overview.jsx
+++ react_gas_app/client/src/components/Overview/Overview.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './App.css'
+import Sidebar from '../Overview/Components/Sidebar Section/Sidebar'
+import Body from '../Overview/Components/Body Section/Body'
 
 const Overview = () => {
   return (
-    <div>
-      Overview
+    <div className='overview flex'>
+      <div className='overviewContainer flex'>
+        <Sidebar/>
+        <Body/>
+      </div>
     </div>
   )
 }
