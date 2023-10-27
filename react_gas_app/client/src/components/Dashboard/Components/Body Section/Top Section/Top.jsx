d1af2a600d44ac745ac6c3f3318764922ac5c096
waxx567 <waynem567@gmail.com>
2023-10-27 08:59:32 +0200

    Trying to set up Google Cloud for VPS
    Virtual Private Server

--- react_gas_app/client/src/components/Dashboard/Components/Body Section/Top Section/Top.jsx
+++ react_gas_app/client/src/components/Dashboard/Components/Body Section/Top Section/Top.jsx
@@ -8,7 +8,7 @@ import { MdOutlineNotificationsNone } from 'react-icons/md'
 import { BsArrowRightShort, BsQuestionCircle } from 'react-icons/bs'
 
 // Import media
-import img from '../../../Assets/owl1.png'
+import img from '../../../Assets/grumpycat.png'
 import img2 from '../../../Assets/owl2.png'
 import video from '../../../Assets/owl.mp4'
 
